50dd06f043d6c73b29e360f1a609aa52cce90db5
Dan Gordon <gordond@byui.edu>
2023-07-10 14:47:22 -0600

    Start from template

---
+++ Shared/Models/CoreBelief.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreBeliefsSurvey.Shared.Models
+{
+    public class CoreBelief
+    {
+        public bool IsPositive { get; set; }
+        public string BeliefName { get; set; }
+        public string BeliefDescription { get; set; }
+    }
+}
---
+++ Shared/Services/BeliefService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using CsvHelper;
+using Azure.Data.Tables;
+
+namespace CoreBeliefsSurvey.Shared.Services
+{
+    public class BeliefService
+    {
+
+    // Instantiate a TableClient which will be used to create and manipulate the table
+    string connectionString = "<Your Azure Storage Connection String>";
+    string tableName = "<Your Table Name>";
+    var tableClient = new TableClient(connectionString, tableName);
+
+    // Create the table if it doesn't exist
+    await tableClient.CreateIfNotExistsAsync();
+
+    // Path to the CSV file
+    string path = "coreBeliefs.csv";
+
+using (var reader = new StreamReader(path))
+using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+{
+    // Get records from CSV file
+    var records = csv.GetRecords<BeliefRecord>();
+
+    // Loop through each record
+    foreach (var record in records)
+    {
+        // Create a new TableEntity for each record
+        TableEntity beliefEntity = new TableEntity(record.PartitionKey, record.RowKey)
+        {
+            { "Belief", record.Belief }
+        };
+
+    // Add the entity to the table
+    await tableClient.AddEntityAsync(beliefEntity);
+}
+}
+
+    }
+}
