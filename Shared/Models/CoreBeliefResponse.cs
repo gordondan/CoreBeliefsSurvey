6a3f3c07c24c2fc0c5bb097b22f61e0ee997d978
Dan Gordon <gordond@byui.edu>
2023-07-11 16:18:49 -0600

    progress

---
+++ Shared/Models/CoreBeliefResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreBeliefsSurvey.Shared.Models
+{
+    public class CoreBeliefResponse
+    {
+        public int SelectedValue { get; set; }
+        public CoreBelief Belief { get; set; }
+    }
+}
